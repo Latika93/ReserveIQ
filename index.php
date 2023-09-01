ca32a8525f059f44cb926ced8e1373fe76efb476
Latika <vermalatika8@gmail.com>
2023-09-01 12:01:59 +0530

    update home bar

--- index.php
+++ index.php
@@ -29,6 +29,13 @@
         input[type=number] {
         -moz-appearance: textfield;
         }
+
+        .custom-bg{
+            background-color: #2ec1ac;
+        }
+        .custom-bg: hover{
+            background-color: #279e8c;
+        }
     </style>
 </head>
 <body>
@@ -189,6 +196,7 @@
         </div>
     </div>
 
+    <!-- swiper -->
     <div class="container-fluid">
         <swiper-container>
             <swiper-slide lazy="true">
@@ -204,7 +212,55 @@
         </swiper-container>
     </div>
 
+    <!-- check availability form -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 bg-white shadow p-4 rounded">
+                <h5 class="mb-4">Check Booking availability</h5>
+                <form action="">
+                    <div class="row align-items-end">
+                        <div class="col-lg-3">
+                            <label for="" class="form-label" style="font-weight: 500;">Check-in</label>
+                            <input type="date" class="form-control shadow-none">
+                        </div>
+                        <div class="col-lg-3">
+                            <label for="" class="form-label" style="font-weight: 500;">Check-out</label>
+                            <input type="date" class="form-control shadow-none">
+                        </div>
+                        <div class="col-lg-3">
+                            <label for="" class="form-label" style="font-weight: 500;">Adult</label>
+                            <select class="form-select shadow-none" aria-label="Default select example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-2">
+                            <label for="" class="form-label" style="font-weight: 500;">Children</label>
+                            <select class="form-select shadow-none" aria-label="Default select example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="col-lg-1">
+                            <button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <br><br><br>
+    <br><br><br>
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<!-- 02 || 51:44 -->
